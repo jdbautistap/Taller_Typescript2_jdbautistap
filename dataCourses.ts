09cd6832069912e270b064454a2719c49a5f83cc
jdbautistap <jd.bautistap@uniandes.edu.co>
2020-10-23 23:48:51 -0500

    taller finalizado

---
+++ dataCourses.ts
@@ -0,0 +1,13 @@
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Portugues", "Karina", 3),
+  new Course("Sistemas transaccionales","German ", 3),
+  new Course("Desarrollo de SW en equipo", "Cesar", 3),
+  new Course("Algebra Lineal", "Carolina", 3),
+  new Course("Física 2", "Mayerlin", 3),
+  new Course("Arquitectura Empresarial", "Oscar", 3),
+  new Course("Biología", "Maria", 3),
+  new Course("CBU1", "Hernando", 2),
+  new Course("CBU2", "Sebastian", 2)
+]
---
+++ scripts/dataStudents.js
@@ -0,0 +1,5 @@
+import { Student } from './student.js';
+
+export const dataStudent = [
+  new Student(201812764, 1032502361, 21, "Una casa", 3227889797)
+]
